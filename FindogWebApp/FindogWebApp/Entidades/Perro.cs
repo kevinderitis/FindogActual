0ef3fe6f477f0f57c81f55f6556b7c88e44f3b29
37246836 <37246836@A-CEO-04>
2018-09-05 21:57:12 -0300

    Cambios

---
+++ FindogWebApp/FindogWebApp/Entidades/Perro.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Perro
+    {
+        private int idPerro { get; set; }
+        private string color { get; set; }
+        private string tamaño  { get; set; }
+        private string raza { get; set; }
+        private string zona{ get; set; }
+        private string calle1 { get; set; }
+        private string calle2 { get; set; }
+
+        private Usuario usuario{ get; set; }
+        private byte imagen{ get; set; }
+
+
+        public Perro(string color, string tamaño, string raza, string zona, string calle1, string calle2, Usuario usuario, byte imagen)
+        {
+            this.color = color;
+            this.tamaño = tamaño;
+            this.raza = raza;
+            this.calle1 = calle1;
+            this.calle2 = calle2;
+            this.usuario = usuario;
+            this.imagen = imagen;
+            
+        }
+
+
+
+    }
+}
---
+++ FindogWebApp/FindogWebApp/Entidades/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Usuario
+    {
+        private int idUsuario { get; set; } 
+        private string usuario { get; set; }
+        private string password { get; set; }
+        private string telefono { get; set; }
+        private string mail { get; set; }
+        private List<Perro> perrosEncontrados { get; set; }
+
+        public Usuario(string usuario,string password,string telefono, string mail)
+        {
+            this.idUsuario= 0; // consultar como hacer con ID
+            this.usuario = usuario;
+            this.password = password;
+            this.telefono = telefono;
+            this.mail = mail;
+            this.perrosEncontrados = new List<Perro>();
+        }
+
+    }
+}
