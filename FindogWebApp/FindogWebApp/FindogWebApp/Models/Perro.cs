8a5cd8718baf7f59689b1ce78b21e21f82bd0a7c
Kevin De Ritis <kevin.deritis@hotmail.com>
2018-09-07 12:50:23 -0300

    Cambios 07/09

---
+++ FindogWebApp/FindogWebApp/FindogWebApp/Models/Perro.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FindogWebApp.Models
+{
+    public class Perro
+    {
+
+        [Key]
+        private int Id { get; set; }
+        private string Color { get; set; } 
+        private string Tamaño { get; set; }
+        private string Raza { get; set; }
+        private string Zona { get; set; }
+        private string Calle1 { get; set; }
+        private string Calle2 { get; set; }
+
+        private Usuario Usuario { get; set; }
+        private byte Imagen { get; set; }
+
+
+        public Perro(string color, string tamaño, string raza, string zona, string calle1, string calle2, Usuario usuario, byte imagen)
+        {
+            this.Color = color;
+            this.Tamaño = tamaño;
+            this.Raza = raza;
+            this.Calle1 = calle1;
+            this.Calle2 = calle2;
+            this.Usuario = usuario;
+            this.Imagen = imagen;
+
+        }
+    }
+}
--- FindogWebApp/FindogWebApp/FindogWebApp/Models/Usuario.cs
+++ FindogWebApp/FindogWebApp/FindogWebApp/Models/Usuario.cs
@@ -1,29 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 
-namespace Entidades
+namespace FindogWebApp.Models
 {
     public class Usuario
     {
-        private int idUsuario { get; set; } 
+        
+        private int Id { get; set; }
         private string usuario { get; set; }
         private string password { get; set; }
         private string telefono { get; set; }
         private string mail { get; set; }
         private List<Perro> perrosEncontrados { get; set; }
 
-        public Usuario(string usuario,string password,string telefono, string mail)
+        public Usuario(string usuario, string password, string telefono, string mail)
         {
-            this.idUsuario= 0; // consultar como hacer con ID
+
             this.usuario = usuario;
             this.password = password;
             this.telefono = telefono;
             this.mail = mail;
             this.perrosEncontrados = new List<Perro>();
         }
+
+
+
+
 
     }
 }
